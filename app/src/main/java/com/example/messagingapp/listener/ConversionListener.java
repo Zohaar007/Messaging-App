1b87900babb34d093f28f56821600519369f14db
Zohaar Hembram <zohaarhembram1997@gmail.com>
2022-03-10 18:32:25 +0530

    Final Version

---
+++ app/src/main/java/com/example/messagingapp/listener/ConversionListener.java
@@ -0,0 +1,7 @@
+package com.example.messagingapp.listener;
+
+import com.example.messagingapp.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
---
+++ app/src/main/java/com/example/messagingapp/listener/UserListener.java
@@ -0,0 +1,8 @@
+package com.example.messagingapp.listener;
+
+import com.example.messagingapp.models.User;
+
+public interface UserListener {
+
+    void onUserClicked(User user);
+}
